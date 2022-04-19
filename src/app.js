d7f1d4b349903694d06d5eb484815f405cb29946
Italo Cavalcanti <87244758+italocavalcanti3@users.noreply.github.com>
2022-04-18 22:17:00 -0300

    criando recursos para thumbnail, gerar um url virtual para visualizar a imagem como um link no projeto

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ const express = require('express');
 const routes = require('./routes');
 */
 
-import mongoose from 'mongoose';
 import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
 import routes from './routes';
 
 class App{
@@ -24,6 +25,12 @@ class App{
     }
 
     middlewares(){
+
+        this.server.use(
+            '/files',
+            express.static(path.resolve(__dirname, '..', 'uploads'))
+        );
+
         this.server.use(express.json());
     }
 
